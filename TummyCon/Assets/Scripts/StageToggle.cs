171e017cad4ed5e72a34121b0e23e148b543466f
Tummy <akoj93@gmail.com>
2026-08-03 14:15:37 +0200

    Updates

---
+++ TummyCon/Assets/Scripts/StageToggle.cs
@@ -0,0 +1,31 @@
+﻿
+using UdonSharp;
+using UnityEngine;
+using UnityShaderParser.HLSL;
+using VRC.SDKBase;
+using VRC.Udon;
+
+public class StageToggle : UdonSharpBehaviour
+{
+    public GameObject[] _stageParts;
+    [HideInInspector] public int dialCurrentPosition;
+    [HideInInspector] public float dialCurrentAngle;
+    
+    public void OnDialChanged()
+    {
+        Debug.Log($"[Dial Target] Position: {dialCurrentPosition} | Angle: {dialCurrentAngle}");
+
+        switch (dialCurrentPosition)
+        {
+            case 0:
+                foreach(GameObject stagePart in _stageParts) stagePart.SetActive(false);
+                break;
+            case 1:
+                foreach (GameObject stagePart in _stageParts) stagePart.SetActive(true);
+                break;
+        }
+    }
+    
+    
+    
+}
